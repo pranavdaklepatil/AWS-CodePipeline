21c38ad66a9f1ffa241596b6117a5f86e68a7cde
Pranav Dakle <pranvdakle15@gmail.com>
2026-04-20 12:23:23 +0530

    VPC added

---
+++ infrastructure/vpc.tf
@@ -0,0 +1,19 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "${var.project_name}-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Project = var.project_name
+  }
+}
