ca0b6a7a1392baac66b9324a233d3227d186938c
Pranav Dakle <pranvdakle15@gmail.com>
2026-04-22 12:19:58 +0530

    Added

---
+++ infrastructure/output.tf
@@ -0,0 +1,41 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "eks_cluster_name" {
+  description = "EKS cluster name"
+  value       = var.eks_cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  description = "EKS cluster API endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_ca" {
+  description = "EKS cluster certificate authority data"
+  value       = module.eks.cluster_certificate_authority_data
+  sensitive   = true
+}
+
+output "docdb_endpoint" {
+  description = "DocumentDB endpoint"
+  value       = aws_docdb_cluster.main.endpoint
+  sensitive   = true
+}
+
+output "frontend_ecr_url" {
+  description = "Frontend ECR repository URL"
+  value       = aws_ecr_repository.frontend.repository_url
+}
+
+output "backend_ecr_url" {
+  description = "Backend ECR repository URL"
+  value       = aws_ecr_repository.backend.repository_url
+}
+
+output "aws_region" {
+  description = "AWS region"
+  value       = var.aws_region
+}
