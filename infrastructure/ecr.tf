de3e0bc8e51f7bb458c735b4b5ee599c9987e1ff
Pranav Dakle <pranvdakle15@gmail.com>
2026-04-24 13:02:33 +0530

    Updated for SM

--- infrastructure/ecr.tf
+++ infrastructure/ecr.tf
@@ -1,3 +1,4 @@
+# Frontend ECR Repository
 resource "aws_ecr_repository" "frontend" {
   name                 = var.ecr_frontend_name
   image_tag_mutability = var.ecr_image_tag_mutability
@@ -5,13 +6,33 @@ resource "aws_ecr_repository" "frontend" {
   image_scanning_configuration {
     scan_on_push = var.ecr_scan_on_push
   }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  tags = {
+    Name        = "${var.project_name}-frontend-ecr"
+    Environment = var.environment
+  }
 }
 
+
+#Backend ECR Repository
 resource "aws_ecr_repository" "backend" {
   name                 = var.ecr_backend_name
   image_tag_mutability = var.ecr_image_tag_mutability
 
   image_scanning_configuration {
     scan_on_push = var.ecr_scan_on_push
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  tags = {
+    Name        = "${var.project_name}-backend-ecr"
+    Environment = var.environment
   }
 }
