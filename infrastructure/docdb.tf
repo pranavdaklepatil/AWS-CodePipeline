77c2077e55112d722520b6fe8d9dcf0d3be2fb7a
Pranav Dakle <pranvdakle15@gmail.com>
2026-04-24 13:02:21 +0530

    Updated for SM

--- infrastructure/docdb.tf
+++ infrastructure/docdb.tf
@@ -1,6 +1,19 @@
+data "aws_secretsmanager_secret_version" "docdb" {
+  secret_id = "medgrid/docdb"
+}
+
+locals {
+  docdb_secret = jsondecode(data.aws_secretsmanager_secret_version.docdb.secret_string)
+}
+
 resource "aws_docdb_subnet_group" "main" {
   name       = var.docdb_subnet_group_name
   subnet_ids = module.vpc.private_subnets
+  
+   tags = {
+    Name        = "${var.project_name}-docdb-sg"
+    Environment = var.environment
+  }
 }
 
 resource "aws_security_group" "docdb" {
@@ -21,16 +34,29 @@ resource "aws_security_group" "docdb" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+   tags = {
+    Name        = "${var.project_name}-docdb-sg"
+    Environment = var.environment
+  }
 }
 
 resource "aws_docdb_cluster" "main" {
   cluster_identifier      = var.docdb_cluster_identifier
   engine                  = var.docdb_engine
-  master_username         = var.db_master_username
-  master_password         = var.db_master_password
-  db_subnet_group_name    = aws_docdb_subnet_group.main.name
-  vpc_security_group_ids  = [aws_security_group.docdb.id]
-  skip_final_snapshot     = var.docdb_skip_final_snapshot
+
+  master_username = local.docdb_secret.username
+  master_password = local.docdb_secret.password
+
+  db_subnet_group_name   = aws_docdb_subnet_group.main.name
+  vpc_security_group_ids = [aws_security_group.docdb.id]
+  skip_final_snapshot    = var.docdb_skip_final_snapshot
+
+  tags = {
+    Name        = "${var.project_name}-docdb-cluster"
+    Environment = var.environment
+  }
+
 }
 
 resource "aws_docdb_cluster_instance" "main" {
@@ -38,4 +64,9 @@ resource "aws_docdb_cluster_instance" "main" {
   identifier         = "${var.docdb_instance_identifier_prefix}-${count.index}"
   cluster_identifier = aws_docdb_cluster.main.id
   instance_class     = var.db_instance_class
+
+  tags = {
+    Name        = "${var.project_name}-docdb-instance-${count.index}"
+    Environment = var.environment
+  }
 }
