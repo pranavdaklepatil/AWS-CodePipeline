25a82c33c70d9d50ea2f0116e24cc79fd193b833
Pranav Dakle <pranvdakle15@gmail.com>
2026-04-25 12:26:09 +0530

    Updated

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,10 +1,8 @@
-# VPC
 output "vpc_id" {
   description = "VPC ID"
   value       = module.vpc.vpc_id
 }
 
-# EKS
 output "eks_cluster_name" {
   description = "EKS cluster name"
   value       = var.eks_cluster_name
@@ -21,19 +19,17 @@ output "eks_cluster_ca" {
   sensitive   = true
 }
 
-# DocumentDB
 output "docdb_endpoint" {
-  description = "DocumentDB cluster endpoint"
+  description = "DocumentDB endpoint"
   value       = aws_docdb_cluster.main.endpoint
   sensitive   = true
 }
 
 output "docdb_secret_arn" {
-  description = "Secrets Manager ARN for DocumentDB credentials"
+  description = "Secrets Manager ARN"
   value       = aws_secretsmanager_secret.docdb.arn
 }
 
-# ECR
 output "frontend_ecr_url" {
   description = "Frontend ECR repository URL"
   value       = aws_ecr_repository.frontend.repository_url
@@ -44,7 +40,6 @@ output "backend_ecr_url" {
   value       = aws_ecr_repository.backend.repository_url
 }
 
-# AWS Region
 output "aws_region" {
   description = "AWS region"
   value       = var.aws_region
