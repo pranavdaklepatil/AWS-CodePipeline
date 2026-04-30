954699bbaf4209fda6bf5f6204c55b1ab201095f
Pranav Dakle <pranvdakle15@gmail.com>
2026-04-30 12:15:10 +0530

    updated

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -1,3 +1,5 @@
+data "aws_availability_zones" "available" {}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
@@ -5,14 +7,15 @@ module "vpc" {
   name = "${var.project_name}-vpc"
   cidr = var.vpc_cidr
 
-  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
+  azs             = azs = slice(data.aws_availability_zones.available.names, 0, 2)
   private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
 
   # NAT Gateway
   enable_nat_gateway     = true
-  single_nat_gateway     = false
-  one_nat_gateway_per_az = true
+  single_nat_gateway     = true 
+
+   map_public_ip_on_launch = true
 
   # DNS (REQUIRED for EKS)
   enable_dns_hostnames = true
@@ -21,12 +24,12 @@ module "vpc" {
   # EKS Subnet Tags 
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb"           = "1"
-    "kubernetes.io/cluster/${var.project_name}" = "owned"
+    "kubernetes.io/cluster/${var.project_name}-eks-cluster" = "owned"
   }
 
   public_subnet_tags = {
     "kubernetes.io/role/elb"                    = "1"
-    "kubernetes.io/cluster/${var.project_name}" = "owned"
+    "kubernetes.io/cluster/${var.project_name}-eks-cluster" = "owned"
   }
 
   # Flow Logs (Monitoring)
@@ -34,6 +37,7 @@ module "vpc" {
   create_flow_log_cloudwatch_log_group = true
   create_flow_log_cloudwatch_iam_role  = true
   flow_log_max_aggregation_interval    = 60
+  flow_log_cloudwatch_log_group_retention_in_days = 7
 
   
   enable_vpn_gateway = false
